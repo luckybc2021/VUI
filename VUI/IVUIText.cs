24f36a35dd04a405b97124eeb71d5ec849089849
vosongtoan <vosongtoan@outlook.com>
2024-05-29 09:23:07 -0700

    Add IVUIText interface

---
+++ VUI/IVUIText.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VUI
+{
+    public interface IVUIText
+    {
+        string FontFamily { get; set; }
+        string FontSize { get; set; }
+        string FontWeight { get; set; }
+    }
+}
