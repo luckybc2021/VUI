60dcf400226bc5b45787feeab8e40995358053ec
vosongtoan <vosongtoan@outlook.com>
2024-05-29 01:05:38 -0700

    Add VUIList.razor and VUIList.razor.cs

---
+++ VUI/VUIList.razor.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Components;
+
+namespace VUI
+{
+    public partial class VUIList<TItem>
+    {
+        [Parameter]
+        public RenderFragment<TItem> ChildContent { get; set; } = default!;
+
+        [Parameter, EditorRequired]
+        public IReadOnlyList<TItem> Items { get; set; } = default!;
+
+
+        private string fontFamily = "arial";
+        [Parameter]
+        public string FontFamily
+        {
+            get => fontFamily;
+            set
+            {
+                fontFamily = value;
+            }
+        }
+
+        private string fontSize = "initial";
+        [Parameter]
+        public string FontSize
+        {
+            get => fontSize;
+            set
+            {
+                fontSize = value;
+            }
+        }
+
+        private string fontWeight = "normal";
+        [Parameter]
+        public string FontWeight
+        {
+            get => fontWeight;
+            set
+            {
+                fontWeight = value;
+            }
+        }
+
+
+        protected override void OnInitialized()
+        {
+            ContentType = "List";
+            Display = "block";
+            Position = "relative";
+            
+            base.OnInitialized();
+        }
+    }
+}
